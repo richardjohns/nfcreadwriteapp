0ca905a31735cd2a9092f0073fa4c89a4115a704
richardjohns <you@example.com>
2017-10-12 11:02:56 +1300

    sorted adding plugin

---
+++ platforms/android/assets/www/cordova_plugins.js
@@ -0,0 +1,26 @@
+cordova.define('cordova/plugin_list', function(require, exports, module) {
+module.exports = [
+  {
+    "id": "cordova-plugin-statusbar.statusbar",
+    "file": "plugins/cordova-plugin-statusbar/www/statusbar.js",
+    "pluginId": "cordova-plugin-statusbar",
+    "clobbers": [
+      "window.StatusBar"
+    ]
+  },
+  {
+    "id": "phonegap-nfc.NFC",
+    "file": "plugins/phonegap-nfc/www/phonegap-nfc.js",
+    "pluginId": "phonegap-nfc",
+    "runs": true
+  }
+];
+module.exports.metadata = 
+// TOP OF METADATA
+{
+  "cordova-plugin-statusbar": "2.1.3",
+  "cordova-plugin-whitelist": "1.3.2",
+  "phonegap-nfc": "0.7.0"
+};
+// BOTTOM OF METADATA
+});
--- platforms/ios/platform_www/cordova_plugins.js
+++ platforms/ios/platform_www/cordova_plugins.js
@@ -7,13 +7,20 @@ module.exports = [
     "clobbers": [
       "window.StatusBar"
     ]
+  },
+  {
+    "id": "phonegap-nfc.NFC",
+    "file": "plugins/phonegap-nfc/www/phonegap-nfc.js",
+    "pluginId": "phonegap-nfc",
+    "runs": true
   }
 ];
 module.exports.metadata = 
 // TOP OF METADATA
 {
   "cordova-plugin-whitelist": "1.3.2",
-  "cordova-plugin-statusbar": "2.1.3"
+  "cordova-plugin-statusbar": "2.1.3",
+  "phonegap-nfc": "0.7.0"
 };
 // BOTTOM OF METADATA
 });
