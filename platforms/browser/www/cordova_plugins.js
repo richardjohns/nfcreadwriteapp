c251e9a9347b4d96f5e4c85ebb5f755b95c32454
richardjohns <you@example.com>
2017-10-14 17:52:49 +1300

    config changes

---
+++ platforms/browser/www/cordova_plugins.js
@@ -0,0 +1,28 @@
+cordova.define('cordova/plugin_list', function(require, exports, module) {
+module.exports = [
+    {
+        "file": "plugins/cordova-plugin-statusbar/www/statusbar.js",
+        "id": "cordova-plugin-statusbar.statusbar",
+        "pluginId": "cordova-plugin-statusbar",
+        "clobbers": [
+            "window.StatusBar"
+        ]
+    },
+    {
+        "file": "plugins/cordova-plugin-statusbar/src/browser/statusbar.js",
+        "id": "cordova-plugin-statusbar.statusbar.Browser",
+        "pluginId": "cordova-plugin-statusbar",
+        "merges": [
+            "window.StatusBar"
+        ]
+    }
+];
+module.exports.metadata = 
+// TOP OF METADATA
+{
+    "cordova-plugin-statusbar": "2.1.3",
+    "cordova-plugin-whitelist": "1.3.2",
+    "phonegap-nfc": "0.7.0"
+}
+// BOTTOM OF METADATA
+});
--- src/components/Hello.js
+++ src/components/Hello.js
@@ -5,19 +5,40 @@ import '../css/hello.css';
 
 import Message from './Message';
 
+// set initial state in this.state. 
+// Find what initial nfc object state should be - empty object?
+// set event listener into componentdidmount?
+// how will event listener return intent?
+// bind all needed functions in constructor.
+// if all else fails... set up fake nfc event.
+
 class Hello extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
       displayName: 'Hello world',
       message: '',
+      nfcmsg: 'No NFC tag found'
     }
   }
 
-  getInitialState() {
-    return {
-      message: '',
-    };
+  nfc.addMimeTypeListener("text/json", *onNfc *, success, failure);
+
+
+  // window.addEventListener is to listen for events handled in the DOM for which there are no synthetic event equivalents.
+  // 1st arg: event you want to listen for. 2nd arg: function that will handle the event.
+  componentDidMount() {
+    window.addEventListener("resize", this.handleResizedScreen.bind(this));
+  }
+
+  componentWillUnmount() {
+    window.removeEventListener("resize", this.handleResizedScreen.bind(this));
+  }
+
+  nfcstatus(nfc) {
+    this.setState({
+      nfcmsg: nfc
+    });
   }
 
   sayHello(msg) {
@@ -37,6 +58,8 @@ class Hello extends React.Component {
     return (
       <div className="hello">
         <Message message={ this.state.message } />
+        <br/>
+        <p className="hello-message">{this.state.nfcmsg}</p>
         <Tappable
           className="button-say-hello"
           onTap={() => this.sayHello(this.state.displayName)}
